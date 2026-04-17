a261fa7a5de91e0c4fe9899c14dbd5986d7d42a0
AnandNarra <narra9505@gmail.com>
2026-04-17 20:17:58 +0530

    added navbar pink angels into angel

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
 
   const links = [
     { name: 'Home', path: '/' },
-    { name: 'Pink Angels', path: '/pink-angel' },
+    { name: 'Pink Angel', path: '/pink-angels' },
     { name: 'Training', path: '/training' },
     { name: 'Franchise', path: '/franchise' },
     { name: 'Equipments', path: '/gym-equipments' },
